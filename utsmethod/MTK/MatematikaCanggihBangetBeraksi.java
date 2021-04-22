af731680873dda22bf62eec800f5d16d43013437
AbdillahAlMushauwir <80298278+AbdillahAlMushauwir@users.noreply.github.com>
2021-04-22 09:15:14 +0700

    Add files via upload

---
+++ utsmethod/MTK/MatematikaCanggihBangetBeraksi.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MTK;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class MatematikaCanggihBangetBeraksi {
+    public static void main(String []args){
+
+        MatematikaCanggihBanget mtkku = new MatematikaCanggihBanget();
+
+        mtkku.pertambahan(9,26, 5);
+        mtkku.perkalian(17,23, 2);
+        mtkku.modulus(75,15);
+        mtkku.pertambahanEmpat(6,19,11,24);
+    }
+}
